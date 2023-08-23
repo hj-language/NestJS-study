1c412112558bc8b11fce03e43cf955adbd76a0e0
hj-language <idgafff010@gmail.com>
2023-08-23 20:43:20 +0900

    feat: 테스트용 api 작성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDto } from './dto/create.user.dto';
+import { CreateUserExtraDto } from './dto/create.user.extra.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  createUser(@Body() dto: CreateUserDto | CreateUserExtraDto) {
+    if ('school' in dto) {
+      console.log(dto); // dto 타입: CreateUserExtraDto
+      let specificDto: CreateUserExtraDto = dto as CreateUserExtraDto;
+      return specificDto;
+    } else {
+      console.log(dto); // dto 타입: CreateUserDto
+      let specificDto: CreateUserDto = dto as CreateUserDto;
+      return specificDto;
+    }
   }
 }
